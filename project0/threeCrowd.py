67a98b2010f1144d51fb5525129555ccfbad0ff7
Natalie Westermann <nataliewestermann@yahoo.com>
2021-10-07 13:32:50 -0700

    If Statement Exercises

---
+++ project0/threeCrowd.py
@@ -0,0 +1,10 @@
+names = ['Daisy', 'Delilah', 'Rose', 'Poppy']
+if len(names) > 3:
+    print("Wow! This room is so crowded.")
+
+del names[0]
+
+del names[2]
+
+if len(names) > 3:
+    print("Wow! This room is so crowded.")
