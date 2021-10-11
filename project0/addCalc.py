026af472ad01695bcea2e83a43583effaaa85fb2
Natalie Westermann <nataliewestermann@yahoo.com>
2021-10-11 11:08:03 -0700

    addCalc.py

---
+++ project0/addCalc.py
@@ -0,0 +1,11 @@
+def get_number_sum(number1, number2):
+
+    addition = number1+number2
+    print("The sum of " + str(number1) + " and " + str(number2) + " is " + str(addition))
+
+
+get_number_sum(3, 4)
+
+
+
+
